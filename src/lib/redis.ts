1fe749aadfae8c3ae97784061300aa428e95861c
eatdeliciousbanana <93957952+eatdeliciousbanana@users.noreply.github.com>
2025-06-26 18:12:45 +0900

    refactor

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,9 +1,9 @@
 import { DashboardData } from "@/types/spotify";
 import { Redis } from "@upstash/redis";
 
-const SPOTIFY_ACCESS_TOKEN_KEY = "spotify_jam_access_token";
-const SPOTIFY_REFRESH_TOKEN_KEY = "spotify_jam_refresh_token";
-const DASHBOARD_DATA_KEY = "spotify_jam_dashboard_data";
+const ACCESS_TOKEN_KEY = "spotify_jam:access_token";
+const REFRESH_TOKEN_KEY = "spotify_jam:refresh_token";
+const DASHBOARD_DATA_KEY = "spotify_jam:dashboard_data";
 
 const redis = Redis.fromEnv();
 
@@ -12,24 +12,24 @@ export const storeTokens = async (
   expiresIn: number,
   refreshToken?: string
 ) => {
-  await redis.set(SPOTIFY_ACCESS_TOKEN_KEY, accessToken, { ex: expiresIn });
+  await redis.set(ACCESS_TOKEN_KEY, accessToken, { ex: expiresIn });
   if (refreshToken) {
-    await redis.set(SPOTIFY_REFRESH_TOKEN_KEY, refreshToken);
+    await redis.set(REFRESH_TOKEN_KEY, refreshToken);
   }
 
   return accessToken;
 };
 
 export const refreshTokenExists = async () => {
-  return (await redis.exists(SPOTIFY_REFRESH_TOKEN_KEY)) > 0;
+  return (await redis.exists(REFRESH_TOKEN_KEY)) > 0;
 };
 
 export const getRefreshToken = async () => {
-  return await redis.get<string>(SPOTIFY_REFRESH_TOKEN_KEY);
+  return await redis.get<string>(REFRESH_TOKEN_KEY);
 };
 
 export const getAccessToken = async () => {
-  return await redis.get<string>(SPOTIFY_ACCESS_TOKEN_KEY);
+  return await redis.get<string>(ACCESS_TOKEN_KEY);
 };
 
 export const getDashboardData = async () => {
